431667d930cff4894b971f3a5d9c7793df755efa
Andrés Gas <andresgasdev@gmail.com>
2020-09-06 18:17:17 +0200

    Add docs

---
+++ docs/precache-manifest.c4059267f9b7baeece72d488f88930f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09b967b304020ba566b533c6bce8733",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa40cdb57875a7638535",
+    "url": "/static/css/main.2906266a.chunk.css"
+  },
+  {
+    "revision": "f4f94b6f63ef6d271e4b",
+    "url": "/static/js/2.527f945a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.527f945a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa40cdb57875a7638535",
+    "url": "/static/js/main.6e88af55.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
